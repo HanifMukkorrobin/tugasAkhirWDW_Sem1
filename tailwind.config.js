d76334e69868dde6a542c0b015bee33e17cb4dce
HanifMukkorrobin <hanifmukkorrobin@gmail.com>
2023-11-29 19:33:19 +0700

    setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
